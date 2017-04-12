38e2800108fbd03d1c3593906a80acab75e69bbd
matteo.camilli <matteo.camilli@unimi.it>
2017-04-12 20:06:09 +0200

    gson library

--- src/main/java/it/unimi/di/sweng/conductor2pn/core/ConductorToPn.java
+++ src/main/java/it/unimi/di/sweng/conductor2pn/core/ConductorToPn.java
@@ -1,5 +1,12 @@
 package it.unimi.di.sweng.conductor2pn.core;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ConductorToPn {
 
     private ConductorToPn(){
@@ -18,6 +25,14 @@ public class ConductorToPn {
         public ConductorToPn build() {
             if(inputWorkflowPath == null)
                 return null;
+            JsonObject json = null;
+            try {
+                json = new JsonParser()
+                        .parse(new FileReader(new File(inputWorkflowPath)))
+                        .getAsJsonObject();
+            } catch (FileNotFoundException e) {
+                return null;
+            }
             return new ConductorToPn();
         }
 
--- src/test/java/it/unimi/di/sweng/conductor2pn/core/ConductorToPnTest.java
+++ src/test/java/it/unimi/di/sweng/conductor2pn/core/ConductorToPnTest.java
@@ -7,8 +7,11 @@ import it.unimi.di.sweng.conductor2pn.core.ConductorToPn.ConductorToPnBuilder;
 
 public class ConductorToPnTest {
 
-    @Test public void testSomeLibraryMethod() {
-        ConductorToPn conductor2PnEngine = new ConductorToPnBuilder().build();
-        assertNull(conductor2PnEngine);
+    @Test
+    public void ConductorToPnBuilderTest() {
+        ConductorToPn conductor2PnEngine = new ConductorToPnBuilder()
+                .setWorkflowPath("src/main/resources/kitchensink.json")
+                .build();
+        assertNotNull(conductor2PnEngine);
     }
 }
